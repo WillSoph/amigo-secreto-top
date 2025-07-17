7fdea13106a455e5f6300983f21fb16dcca5ad13
Willians Ferreira <williansdesigner@hotmail.com>
2025-07-17 13:37:58 -0300

    primeiro commit do sistema amigo secreto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,9 @@ export const metadata = {
   }
 }
 
-export default function RootLayout({ children }) {
+import { ReactNode } from "react";
+
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-br">
       <head>
